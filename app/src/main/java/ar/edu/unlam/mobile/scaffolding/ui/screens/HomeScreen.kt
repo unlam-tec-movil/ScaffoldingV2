fd2b40743f1799671c720f81f9c08a8214b878e1
Thargelion <elwindwalker@gmail.com>
2024-04-13 18:54:28 -0300

    feat: :hammer: WIP initial scaffolding

---
+++ app/src/main/java/ar/edu/unlam/mobile/scaffolding/ui/screens/HomeScreen.kt
@@ -0,0 +1,9 @@
+package ar.edu.unlam.mobile.scaffolding.ui.screens
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun HomeScreen(modifier: Modifier = Modifier) {
+
+}
---
+++ app/src/main/java/ar/edu/unlam/mobile/scaffolding/ui/screens/HomeViewModel.kt
@@ -0,0 +1,5 @@
+package ar.edu.unlam.mobile.scaffolding.ui.screens
+
+class HomeViewModel
+{
+}
