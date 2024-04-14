764e7d20e14da519faed5c09329a23a599225d3e
Thargelion <elwindwalker@gmail.com>
2024-04-14 20:15:55 -0300

    chore: :wrench: code style fixes

--- app/src/test/java/ar/edu/unlam/mobile/scaffolding/ExampleUnitTest.kt
+++ app/src/test/java/ar/edu/unlam/mobile/scaffolding/ExampleUnitTest.kt
@@ -1,8 +1,7 @@
 package ar.edu.unlam.mobile.scaffolding
 
-import org.junit.Test
-
 import org.junit.Assert.*
+import org.junit.Test
 
 /**
  * Example local unit test, which will execute on the development machine (host).
