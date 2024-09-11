fd3279e112fea176ae169f54f0ff2686e9ac321f
Maximiliano De Pietro <maximiliano.deprieto@mercadolibre.com>
2024-09-11 10:25:00 -0300

    feat: :hammer: user screen placeholder an access

--- app/src/main/java/ar/edu/unlam/mobile/scaffolding/ui/components/BotomBar.kt
+++ app/src/main/java/ar/edu/unlam/mobile/scaffolding/ui/components/BotomBar.kt
@@ -2,6 +2,7 @@ package ar.edu.unlam.mobile.scaffolding.ui.components
 
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
@@ -23,6 +24,17 @@ fun BottomBar(controller: NavHostController) {
                 Icon(
                     imageVector = Icons.Default.Home,
                     contentDescription = "Home",
+                    tint = MaterialTheme.colorScheme.primary,
+                )
+            },
+        )
+        NavigationBarItem(
+            selected = navBackStackEntry?.destination?.hierarchy?.any { it.route == "home" } == true,
+            onClick = { controller.navigate("user/test") },
+            icon = {
+                Icon(
+                    imageVector = Icons.Default.Person,
+                    contentDescription = "User",
                     tint = MaterialTheme.colorScheme.primary,
                 )
             },
